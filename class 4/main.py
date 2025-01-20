16a141686a154688ce4e5d8c6ceeecb006738269
Gustavo Barbosa <gustavo.b.antonelli@gmail.com>
2025-01-20 13:43:48 -0300

    Add files via upload

---
+++ class 4/main.py
@@ -0,0 +1,21 @@
+
+
+from flask import Flask, render_template 
+from flask_socketio import SocketIO, send 
+
+app = Flask(__name__) 
+app.config["SECRET"] = "ajuiahfa78fh9f78shfs768fgs7f6" 
+app.config["DEBUG"] = True 
+socketio = SocketIO(app, cors_allowed_origins="*") 
+
+@socketio.on("message") # define que a função abaixo vai ser acionada quando o evento de "message" acontecer
+def gerenciar_mensagens(mensagem):
+    print(f"Mensagem: {mensagem}")
+    send(mensagem, broadcast=True) 
+
+@app.route("/") 
+def home():
+    return render_template("index.html") 
+
+if __name__ == "__main__":
+    socketio.run(app, host='localhost') 
